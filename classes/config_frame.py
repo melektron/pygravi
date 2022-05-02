2dd4b8941c8de5cd1953f0d4bdd434d88f829ef2
MrFlunter <flunterpertinger@tsn.at>
2022-05-02 08:51:01 +0200

    added frame config

--- classes/config_frame.py
+++ classes/config_frame.py
@@ -3,4 +3,27 @@ Author:
 MrFlunter
 """
 
-
+import tkinter as tk
+import tkinter.ttk as ttk
+ 
+class Window:
+    def __init__(self, master):
+        self.master = master
+ 
+        frame = ttk.Frame(self.master)
+ 
+        style = ttk.Style()
+        style.configure("Custom.TButton",foreground="black",
+                                         background="white",
+                                         padding=[10, 10, 10, 10],
+                                         font = "Verdana 12 underline")
+ 
+        button = ttk.Button(frame, text = "Click Me!", style = "Custom.TButton")
+        button.pack()
+ 
+        frame.pack(padx = 5, pady = 5)
+ 
+root = tk.Tk()
+root.geometry("200x150")
+window = Window(root)
+root.mainloop()
