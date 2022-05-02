3b62d25ce70700ccbe8cae3998b22fff396c4f54
melektron <matteo@elektron.work>
2022-05-02 17:28:13 +0200

    tested renderer canvas and added pycache to gitignore

--- classes/render_frame.py
+++ classes/render_frame.py
@@ -15,10 +15,15 @@ class RenderFrame(ttk.Frame):
         self.columnconfigure(0, weight=1)
 
         self.render_canvas = tk.Canvas(self)
+        self.render_canvas.bind("<Button-1>", self.canvas_click)
         self.render_canvas.grid(row=0, column=0, sticky="NSEW")
         self.testoval = self.render_canvas.create_oval(0, 0, 50, 50)
 
-        self.render_canvas.move(self.testoval, 10, 10)
+        self.render_canvas.moveto(self.testoval, 10, 10)
+
+    
+    def canvas_click(self, event):
+        self.render_canvas.moveto(self.testoval, event.x, event.y)
 
 
     
